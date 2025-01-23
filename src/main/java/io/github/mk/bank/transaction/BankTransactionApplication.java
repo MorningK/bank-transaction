c204617d192579e8db1e6a2c098c95b52372f093
Kui Chen <chenkui7@foxmail.com>
2025-01-23 17:03:23 +0800

    reformat

--- src/main/java/io/github/mk/bank/transaction/BankTransactionApplication.java
+++ src/main/java/io/github/mk/bank/transaction/BankTransactionApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BankTransactionApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BankTransactionApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(BankTransactionApplication.class, args);
+  }
 }
