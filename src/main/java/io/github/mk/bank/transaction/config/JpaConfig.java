863408be79aa279ad31d64f7aa143d71c89b8908
Kui Chen <chenkui7@foxmail.com>
2025-01-24 11:04:33 +0800

    rm security

--- src/main/java/io/github/mk/bank/transaction/config/JpaConfig.java
+++ src/main/java/io/github/mk/bank/transaction/config/JpaConfig.java
@@ -1,7 +1,7 @@
 package io.github.mk.bank.transaction.config;
 
-
 import java.time.OffsetDateTime;
+import java.time.ZoneId;
 import java.util.Optional;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,8 +9,10 @@ import org.springframework.data.auditing.DateTimeProvider;
 
 @Configuration
 public class JpaConfig {
+  private static final ZoneId DEFAULT_ZONE_ID = ZoneId.of("+08");
+
   @Bean
   public DateTimeProvider offsetDateTimeProvider() {
-    return () -> Optional.of(OffsetDateTime.now());
+    return () -> Optional.of(OffsetDateTime.now(DEFAULT_ZONE_ID));
   }
 }
