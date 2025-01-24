c80b813c587a1da6eece10cd09938e73fc959f55
Kui Chen <chenkui7@foxmail.com>
2025-01-24 14:43:18 +0800

    fix

--- src/main/java/io/github/mk/bank/transaction/controller/advice/ExceptionAdvice.java
+++ src/main/java/io/github/mk/bank/transaction/controller/advice/ExceptionAdvice.java
@@ -1,6 +1,7 @@
 package io.github.mk.bank.transaction.controller.advice;
 
 import io.github.mk.bank.transaction.exception.BackendException;
+import java.util.stream.Collectors;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,10 +10,10 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ExceptionAdvice {
   @ExceptionHandler
   public Response<?> handleValidationException(MethodArgumentNotValidException exception) {
-    String aggregatedErrors = exception.getBindingResult().getFieldErrors()
-      .stream()
-      .map(err -> err.getField() + " " + err.getDefaultMessage())
-      .collect(Collectors.joining("; "));
+    String aggregatedErrors =
+        exception.getBindingResult().getFieldErrors().stream()
+            .map(err -> err.getField() + " " + err.getDefaultMessage())
+            .collect(Collectors.joining("; "));
     return new Response<>(BackendException.ErrorCode.BAD_REQUEST.getCode(), null, aggregatedErrors);
   }
 }
