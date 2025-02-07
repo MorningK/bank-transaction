c9b0ef952e9e6e38112b9c7f416af965b3ea46ec
Kui Chen <chenkui7@foxmail.com>
2025-02-07 17:12:27 +0800

    fix cache (#15)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - Enhanced transaction processing with improved caching and logging for
    higher performance and reliability.
    - **Tests**
    - Added a comprehensive suite of automated tests to validate the
    behavior of transaction operations and ensure system stability.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

---
+++ src/test/java/io/github/mk/bank/transaction/service/CacheTest.java
@@ -0,0 +1,96 @@
+package io.github.mk.bank.transaction.service;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import io.github.mk.bank.transaction.exception.BackendException;
+import io.github.mk.bank.transaction.model.Transaction;
+import io.github.mk.bank.transaction.repository.TransactionRepository;
+import jakarta.transaction.Transactional;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+@SpringBootTest
+class CacheTest {
+  @Autowired TransactionService transactionService;
+  @MockitoBean TransactionRepository transactionRepository;
+
+  @Test
+  @Transactional
+  void cacheTest() {
+    AtomicLong id = new AtomicLong(1);
+    Mockito.when(transactionRepository.findAll(Pageable.unpaged())).thenReturn(Page.empty());
+    Mockito.when(transactionRepository.save(Mockito.any(Transaction.class)))
+        .then(
+            invocation -> {
+              Transaction result = invocation.getArgument(0, Transaction.class);
+              if (result.getId() != null) {
+                return result;
+              }
+              result.setId(id.getAndIncrement());
+              return result;
+            });
+
+    testQueryUsingCache();
+
+    Transaction transaction = createAndPutCache();
+
+    Transaction updated = updateAndPutCache(transaction);
+
+    deleteAndEvictCache(updated);
+  }
+
+  private void deleteAndEvictCache(Transaction updated) {
+    assertDoesNotThrow(() -> transactionService.delete(updated.getId()));
+    assertTrue(transactionService.transactions(Pageable.unpaged()).isEmpty());
+    Mockito.verify(transactionRepository, Mockito.times(4)).findAll(Pageable.unpaged());
+    Mockito.when(transactionRepository.findById(updated.getId())).thenReturn(Optional.empty());
+    assertThrows(BackendException.class, () -> transactionService.findById(updated.getId()));
+    Mockito.verify(transactionRepository, Mockito.times(1)).findById(updated.getId());
+  }
+
+  private Transaction updateAndPutCache(Transaction transaction) {
+    Transaction updated =
+        assertDoesNotThrow(
+            () ->
+                transactionService.update(
+                    transaction.getId(),
+                    new TransactionService.UpdateTransactionRequest("remark2")));
+    assertTrue(transactionService.transactions(Pageable.unpaged()).isEmpty());
+    Mockito.verify(transactionRepository, Mockito.times(3)).findAll(Pageable.unpaged());
+    Mockito.when(transactionRepository.findById(updated.getId())).thenReturn(Optional.of(updated));
+    assertEquals(updated, assertDoesNotThrow(() -> transactionService.findById(updated.getId())));
+    Mockito.verify(transactionRepository, Mockito.times(0)).findById(updated.getId());
+    return updated;
+  }
+
+  private Transaction createAndPutCache() {
+    TransactionService.CreateTransactionRequest createTransactionRequest =
+        new TransactionService.CreateTransactionRequest("1234567890", 100, "remark");
+    Transaction transaction = transactionService.create(createTransactionRequest);
+    assertTrue(transactionService.transactions(Pageable.unpaged()).isEmpty());
+    Mockito.verify(transactionRepository, Mockito.times(2)).findAll(Pageable.unpaged());
+    Mockito.when(transactionRepository.findById(transaction.getId()))
+        .thenReturn(Optional.of(transaction));
+    assertEquals(
+        transaction, assertDoesNotThrow(() -> transactionService.findById(transaction.getId())));
+    Mockito.verify(transactionRepository, Mockito.times(0)).findById(transaction.getId());
+    return transaction;
+  }
+
+  private void testQueryUsingCache() {
+    assertTrue(transactionService.transactions(Pageable.unpaged()).isEmpty());
+    Mockito.verify(transactionRepository, Mockito.times(1)).findAll(Pageable.unpaged());
+    assertTrue(transactionService.transactions(Pageable.unpaged()).isEmpty());
+    Mockito.verify(transactionRepository, Mockito.times(1)).findAll(Pageable.unpaged());
+  }
+}
